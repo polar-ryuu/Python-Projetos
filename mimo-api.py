01b1c419ca91f0888d6ff6646c4318f020e3ad30
Polar <polar.ryuu@gmail.com>
2026-04-22 13:46:45 -0300

    segundo commit

--- mimo-api.py
+++ mimo-api.py
@@ -14,8 +14,20 @@ def send_message(user_message, thread_id):
 
 current_thread_id = None
 
+print("Welcome! Type your message and press Enter to send.")
+print("Type 'exit' to end the program.")
+print("Type 'new' to switch conversation thread.")
+print("Starting a new thread for you.\n")
+
 while True:
     user_message = input("You: ")
+    if user_message.lower() == "exit":
+        break
+    elif user_message.lower() == "new":
+        current_thread_id = None
+        print("Started a new thread.")
+        continue
+
     response_data = send_message(user_message, current_thread_id)
     latest_message = response_data.get("response")
     current_thread_id = response_data.get("threadId")
